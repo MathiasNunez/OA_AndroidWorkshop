65bb905eb9cff606f6406dc409bb17502d6e9cdb
Mathias Nuñez <mnunez@overactiveinc.com>
2017-08-22 11:19:01 -0300

    Workshop 3

--- AndroidWorkshop/app/src/main/java/com/oa/android_workshop/activities/CompanyDetailsActivity.java
+++ AndroidWorkshop/app/src/main/java/com/oa/android_workshop/activities/CompanyDetailsActivity.java
@@ -1,6 +1,8 @@
 package com.oa.android_workshop.activities;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
@@ -14,29 +16,30 @@ public class CompanyDetailsActivity extends Activity implements View.OnClickList
 
     private ImageView vLogo;
     private TextView vName, vPhone, vAddress, vWebSite;
+    private Company mCompany;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_company_details);
-        Company company = (Company) getIntent().getExtras().getSerializable("company");
+        mCompany = (Company) getIntent().getExtras().getSerializable("company");
         vLogo = (ImageView) findViewById(R.id.logo);
         vName = (TextView) findViewById(R.id.name);
         vAddress = (TextView) findViewById(R.id.address);
         vPhone = (TextView) findViewById(R.id.phone);
         vWebSite = (TextView) findViewById(R.id.website);
-        if (company != null) {
+        if (mCompany != null) {
             Picasso.with(this)
-                    .load(company.getLogoUrl())
+                    .load(mCompany.getLogoUrl())
                     .placeholder(getResources().getDrawable(R.mipmap.ic_launcher))
                     .into(vLogo);
 
-            vName.setText(company.getName());
-            vAddress.setText(company.getAddress());
+            vName.setText(mCompany.getName());
+            vAddress.setText(mCompany.getAddress());
             vAddress.setOnClickListener(this);
-            vPhone.setText(company.getPhone());
+            vPhone.setText(mCompany.getPhone());
             vPhone.setOnClickListener(this);
-            vWebSite.setText(company.getWebSiteUrl());
+            vWebSite.setText(mCompany.getWebSiteUrl());
             vWebSite.setOnClickListener(this);
         }
     }
@@ -46,10 +49,22 @@ public class CompanyDetailsActivity extends Activity implements View.OnClickList
         int id = view.getId();
         switch (id) {
             case R.id.address:
+                String addressUri = "http://maps.google.com/maps?q=loc:" + mCompany.getLatitude() + ","
+                        + mCompany.getLongitude() + " (" + mCompany.getName() + ")";
+                Intent addressIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(addressUri));
+                startActivity(addressIntent);
                 break;
             case R.id.phone:
+                Intent phoneIntent = new Intent(Intent.ACTION_DIAL);
+                phoneIntent.setData(Uri.parse("tel:" + mCompany.getPhone()));
+                startActivity(phoneIntent);
                 break;
             case R.id.website:
+                Intent websiteIntent = new Intent(this, WebViewActivity.class);
+                Bundle bundle = new Bundle();
+                bundle.putString("url", mCompany.getWebSiteUrl());
+                websiteIntent.putExtras(bundle);
+                startActivity(websiteIntent);
                 break;
         }
     }
---
+++ AndroidWorkshop/app/src/main/java/com/oa/android_workshop/activities/WebViewActivity.java
@@ -0,0 +1,22 @@
+package com.oa.android_workshop.activities;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.webkit.WebChromeClient;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class WebViewActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        String url = getIntent().getExtras().getString("url");
+        WebView mWebView = new WebView(this);
+        mWebView.getSettings().setLoadWithOverviewMode(true);
+        mWebView.setWebChromeClient(new WebChromeClient());
+        mWebView.setWebViewClient(new WebViewClient());
+        mWebView.loadUrl(url);
+        setContentView(mWebView);
+    }
+}
