5f976e20128043ac47e6109cbef22c57a4224ca9
Mathias Nuñez <mnunez@overactiveinc.com>
2017-08-28 11:18:08 -0300

    Login

--- AndroidWorkshop/app/src/main/java/com/oa/android_workshop/activities/LoginActivity.java
+++ AndroidWorkshop/app/src/main/java/com/oa/android_workshop/activities/LoginActivity.java
@@ -6,9 +6,12 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RelativeLayout;
 import android.widget.Toast;
 
 import com.oa.android_workshop.R;
+import com.oa.android_workshop.network.ServiceHandler;
+import com.overactiveinc.oarestapi.network.RestApiCallback;
 
 /**
  * Created by mnunez on 8/1/17.
@@ -18,6 +21,7 @@ public class LoginActivity extends Activity {
 
     private Button vLoginBtn;
     private EditText vUsername, vPassword;
+    private RelativeLayout vProgressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,7 +31,7 @@ public class LoginActivity extends Activity {
         vLoginBtn = (Button) findViewById(R.id.login_btn);
         vUsername = (EditText) findViewById(R.id.username);
         vPassword = (EditText) findViewById(R.id.password);
-
+        vProgressBar = (RelativeLayout) findViewById(R.id.progress_bar);
 
         vLoginBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -36,12 +40,31 @@ public class LoginActivity extends Activity {
                         vPassword.getText().toString().length() == 0) {
                     Toast.makeText(LoginActivity.this, "Username and Password are required", Toast.LENGTH_SHORT).show();
                 } else {
-                    Intent intent = new Intent(LoginActivity.this, MainActivity.class);
-                    startActivity(intent);
-                    finish();
+                    vProgressBar.setVisibility(View.VISIBLE);
+                    ServiceHandler serviceHandler = new ServiceHandler(LoginActivity.this);
+                    serviceHandler.doPostLogin(vUsername.getText().toString(), vPassword.getText().toString(),
+                            new RestApiCallback<String>() {
+                                @Override
+                                public void onSuccess(String object) {
+                                    goToHome();
+                                    vProgressBar.setVisibility(View.GONE);
+                                }
+
+                                @Override
+                                public void onError(String message) {
+                                    vProgressBar.setVisibility(View.GONE);
+                                    Toast.makeText(LoginActivity.this, message, Toast.LENGTH_SHORT).show();
+                                }
+                            });
                 }
             }
         });
 
+    }
+
+    private void goToHome() {
+        Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+        startActivity(intent);
+        finish();
     }
 }
--- AndroidWorkshop/app/src/main/java/com/oa/android_workshop/network/ServiceHandler.java
+++ AndroidWorkshop/app/src/main/java/com/oa/android_workshop/network/ServiceHandler.java
@@ -2,6 +2,7 @@ package com.oa.android_workshop.network;
 
 import android.content.Context;
 
+import com.android.volley.Request;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.google.gson.Gson;
@@ -10,6 +11,8 @@ import com.overactiveinc.oarestapi.network.RestApiCallback;
 import com.overactiveinc.oarestapi.network.RestApiHandler;
 
 import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.Arrays;
 import java.util.List;
@@ -44,5 +47,41 @@ public class ServiceHandler extends RestApiHandler {
                         callback.onError(error.getMessage());
                     }
                 }));
+    }
+
+    public void doPostLogin(String username, String password, final RestApiCallback<String> callback) {
+        getQueueInstance();
+        JSONObject body = new JSONObject();
+        try {
+            body.put("username", username);
+            body.put("password", password);
+            mRequestQueue.add(this.createRequest(Request.Method.POST, "http://swiftclass.mebapps.com:3000/login",
+                    body, new Response.Listener<JSONObject>() {
+                        @Override
+                        public void onResponse(JSONObject response) {
+                            try {
+                                int code = response.getInt("code");
+                                if(code == 1){
+                                    callback.onSuccess(null);
+                                }else{
+                                    callback.onError(response.getString("msg"));
+                                }
+
+                            }catch (Exception e){
+                                callback.onError("An error occurred - Invalid credentials");
+                            }
+                        }
+                    }, new Response.ErrorListener() {
+                        @Override
+                        public void onErrorResponse(VolleyError error) {
+                            callback.onError("An error occurred - Invalid credentials");
+                        }
+                    }));
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            callback.onError("An error occurred");
+        }
+
     }
 }
